64da62e65243a88b3b0b012a198d4070c24e3c2f
Paresh Rahool <pareshkhatri131@gmail.com>
2022-03-04 22:51:59 +0500

    1ST COMMIT

---
+++ dogapi.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://api.thedogapi.com/v1/votes"
+
+payload = ""
+headers = {
+  'x-api-key': 'bb8f4110-ed96-4552-a939-25c2f8285837'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
